0045c30c6ac36c13366620c6b6f9c7b0af49445d
Al-Parillon <alparillon@gmail.com>
2018-09-14 12:08:50 -0700

    Added Windows class to deal with Windows and monitoring activities

---
+++ src/ui/Window.java
@@ -0,0 +1,28 @@
+package ui;
+
+import javax.swing.*;
+import java.util.*;
+public class Window extends JFrame {
+
+    public Window()
+    {
+
+    }
+
+    private void setImages()
+    {
+
+    }
+
+    private void setSounds()
+    {
+
+    }
+
+    private void setMenus()
+    {
+
+    }
+
+
+}
